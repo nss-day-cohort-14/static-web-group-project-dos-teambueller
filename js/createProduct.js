dc3e496116db4fca89281602dc98c63ca29c0653
Dave Harms <daveharmswebdev@gmail.com>
2016-05-25 22:44:32 -0500

    added price points and ports of call

--- js/createProduct.js
+++ js/createProduct.js
@@ -1,3 +1,32 @@
-var createProduct = {
-	
-}
+var cruises = [];
+
+var cruiseFactory = function(ship, price, region, nights, portsOfCall) {
+
+	var cruise = {};
+	cruise.ship = ship;
+	cruise.price = price;
+	cruise.region = region;
+	cruise.nights = nights;
+	cruise.portsOfCall = portsOfCall;
+
+	return cruise;
+};
+
+cruises.push(cruiseFactory(
+							'Disney Dream',
+							[1000, 750, 500],
+							'Bahamas',
+							7,
+							['Nassau', 'Castaway Cay', 'St. Thomas', 'St. Kitts']));
+cruises.push(cruiseFactory(
+							'Disney Wonder',
+							[1000, 750, 500],
+							'Alaska',
+							7,
+							['Skagway', 'Juneau', 'Glacier National Park', 'Victoria']));
+cruises.push(cruiseFactory(
+							'Carnival Freedom',
+							[1000, 750, 500],
+							'Caribbean',
+							7,
+							['Cozumel', 'Grand Caymans', 'Montego Bay']));
--- js/displayProduct.js
+++ js/displayProduct.js
@@ -1,30 +1,45 @@
 var displayProduct = {
-	init: function() {
+	init: function(cruises) {
 		this.cacheDom();
 		this.captureTemplate();
-		this.stageClone();
-		this.displayClone();
+		for (var x = 0; x < cruises.length; x++) {
+			this.stageClone(cruises[x]);
+			this.displayClone(cruises[x]);
+		}
 	},
 	cacheDom: function() {
 		this.cruiseDisplayArea = document.getElementById('cruiseDisplayArea');
-		console.log(this.cruiseDisplayArea);
 	},
 	captureTemplate: function() {
 		this.template = document.getElementById('cruiseTemplate');
 		this.templateDivs = this.template.content.querySelectorAll('div');
-		console.log(this.template);
-		console.log(this.templateDivs);
 	},
-	stageClone: function() {
-		this.templateDivs[0].textContent = 'Disney Dream';
-		this.templateDivs[1].textContent = '$1000';
-		console.log(this.templateDivs);
+	stageClone: function(obj) {
+		this.templateDivs[0].textContent = obj.ship;
+		this.templateDivs[1].innerHTML = this.getPricePoints(obj.price);
+		this.templateDivs[2].textContent = obj.region;
+		this.templateDivs[3].textContent = obj.nights;
+		this.templateDivs[4].innerHTML = this.getPorts(obj.portsOfCall);
  	},
  	displayClone: function() {
- 		var clone2 = document.importNode(this.template.content, true)
+ 		var clone2 = document.importNode(this.template.content, true);
  		this.cruiseDisplayArea.appendChild(clone2);
+ 	},
+ 	getPricePoints: function(arg) {
+ 		var string = '';
+ 		for (var x = 0; x < arg.length; x++) {
+ 			string += `<p>${arg[x]}</p>`;
+ 		}
+ 		return string;
+ 	},
+ 	getPorts: function(portsOfCall) {
+ 		var string = '';
+ 		for (var x = 0; x < portsOfCall.length; x++) {
+ 			string += `<p>${portsOfCall[x]}</p>`;
+ 		}
+ 		return string;
  	}
 };
 
-displayProduct.init();
+displayProduct.init(cruises);
 
